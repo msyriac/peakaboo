89bf6d6eb555f5cbe875f2627fc61eb2618cb441
liuxx479 <liuxx479@gmail.com>
2018-05-25 14:56:49 -0400

    fix bug

--- collect_stats.py
+++ collect_stats.py
@@ -8,6 +8,7 @@ fn_list = genfromtxt(peaka_dir+'cosmo_params_all.txt',usecols=1, dtype='string')
 fnjia_list = genfromtxt(peaka_dir+'cosmo_params_all.txt',usecols=0, dtype='string')
 stats_fn = lambda iALL, ieb, icosmo: '/scratch/02977/jialiu/peakaboo/stats/'+icosmo+'/1024b512/'+ieb+'/seed0/'+iALL
 stats_dir = '/scratch/02977/jialiu/peakaboo/stats_avg/'
+stats1k_dir = '/scratch/02977/jialiu/peakaboo/stats_avg_1k/'
 
 def compute_average(i):
     icosmo = fn_list[i]
@@ -24,12 +25,28 @@ def compute_average(i):
             for k in range(10):
                 save(isavedir+iALL[:-4]+'_1k%i'%(k), mean(idata[k*1000:(k+1)*1000],axis=0) )
 
+def compute_average_bystats(iALL):
+    print iALL
+    for ieb in ['output_eb_5000_s4', 'output_tt_3000_s4']: 
+        isavedir = stats_dir+ieb+'/'
+        isavedir_1k = stats1k_dir+ieb+'/'
+        os.system('mkdir -pv %s; mkdir -pv %s'%(isavedir, isavedir_1k))
+        idatagen = lambda icosmo: laod(stats_fn(iALL, ieb, icosmo))
+        all_idata = array(map(idatagen, fn_list))
+        save(isavedir+iALL[:-4]+'_10k', mean(idata,axis=1) )
+        save(isavedir+iALL[:-4]+'_5ka', mean(idata[:5000],axis=1) )
+        save(isavedir+iALL[:-4]+'_5kb', mean(idata[5000:],axis=1) )
+        for k in range(10):
+            save(isavedir_1k+iALL[:-4]+'_1k%i'%(k), mean(idata[k*1000:(k+1)*1000],axis=1) )
+
+        
 pool=MPIPool()
 if not pool.is_master():
     pool.wait()
     sys.exit(0)
     
-pool.map(compute_average, range(len(fn_list)))
+#pool.map(compute_average, range(len(fn_list)))
+pool.map(compute_average_bystats, range(len(ALL_fn)))
 
 pool.close()
 print 'DONE-DONE-DONE'
