630a517f1a90848eec062ecdde0066568053efed
liuxx479 <liuxx479@gmail.com>
2018-06-19 18:17:59 -0400

    prep emulator

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -48,7 +48,7 @@ pdf2dN1ks = array( [[load(eb1k_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_1k{2}.npy'
 
 ##### PS shape:(101,100)
 psI_flat = swapaxes(psI,0,1).reshape(101,-1) 
-psI1k_flats = array([swapaxes(ips,0,1).reshape(101,-1) for ips in psI1ks])
+psI1k_flat = array([swapaxes(ips,0,1).reshape(101,-1) for ips in psI1ks])
 
 psN_cov = swapaxes(array( [load(ebcov_dir+'ALL_galXgal_z{0}_z{0}.npy'.format(iz)) for iz in z_arr]),0,1).reshape(10000,-1)
 covpsN = cov(psN_cov,rowvar=0)*12.25/2e4
@@ -57,8 +57,8 @@ covIpsN = mat(covpsN).I
 ###### PDF 1D
 idxt=where(pdf1dN[:,1]>5)#range(10, 20)#
 
-pdf1dN_flats= swapaxes(pdf1dN[idxt[0],:,idxt[1]],0,1).reshape(101,-1) 
-pdf1dN1k_flats = array([swapaxes(ips[idxt[0],:,idxt[1]],0,1).reshape(101,-1) for ips in pdf1dN1ks])
+pdf1dN_flat= swapaxes(pdf1dN[idxt[0],:,idxt[1]],0,1).reshape(101,-1) 
+pdf1dN1k_flat = array([swapaxes(ips[idxt[0],:,idxt[1]],0,1).reshape(101,-1) for ips in pdf1dN1ks])
 
 pdf1dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_gal_pdf_z{0}_sg1.0.npy'.format(iz)) for iz in z_arr])[idxt[0],:,idxt[1]],0,1).reshape(10000,-1)
 covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
@@ -67,8 +67,8 @@ covIpdf1dN = mat(covpdf1dN).I
 ###### PDF 2D
 idxt2=where(pdf2dN[:,1]>5)
 
-pdf2dN_flats= swapaxes(pdf2dN,0,1)[:,idxt2[0],idxt2[1],idxt2[2]]
-pdf2dN1k_flats= array([swapaxes(ips,0,1)[:,idxt2[0],idxt2[1],idxt2[2]] for ips in pdf2dN1ks])
+pdf2dN_flat= swapaxes(pdf2dN,0,1)[:,idxt2[0],idxt2[1],idxt2[2]]
+pdf2dN1k_flat= array([swapaxes(ips,0,1)[:,idxt2[0],idxt2[1],idxt2[2]] for ips in pdf2dN1ks])
 
 pdf2dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.npy'.format(z_arr[i],z_arr[j]))
                              for i in range(Nz) for j in range(i+1,Nz)]),0,1)[:,idxt2[0],idxt2[1],idxt2[2]].reshape(10000,-1)
@@ -76,3 +76,17 @@ pdf2dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.
 covpdf2dN = cov(pdf2dN_cov,rowvar=0)*12.25/2e4
 covIpdf2dN = mat(covpdf2dN).I
 
+
+#####################################
+###### build emulator ###############
+#####################################
+
+######## pick the good cosmology, where std/P among 10 1k models is <1%, and remove the first cosmology, 0eV one
+psI1k_std = std(psI1ks,axis=0)
+frac_diff = psI1k_std/psI[:,1].reshape(Nz,1,20)
+idx_good = where(amax(mean(frac_diff,axis=-1),axis=0)<0.01)[0][1:] 
+
+stats = [psI_flat, pdf1dN_flat, pdf2dN_flat]
+covIs = [covIpsN, covIpdf1dN, covIpdf2dN]
+
+#WLanalysis.buildInterpolator(istats[idx_good], params[idx_good])
