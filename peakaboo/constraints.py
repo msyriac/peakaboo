b4161ecb77b50f4b7a70e1838365f3621433db05
liuxx479 <liuxx479@gmail.com>
2018-06-23 15:12:25 -0400

    prob grid rand10

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -100,13 +100,13 @@ idx_good = where(amax(mean(frac_diff,axis=-1),axis=0)<0.01)[0][1:]
 ############## test 6/23, use different IC for building emulator ########
 np.random.seed(10027)
 idx10 = list(np.random.randint(0,10, 101))
-stats = [[istats[idx10[i],i] for i in range(101)] for istats in [psI1k_flat, pdf1dN1k_flat, pdf2dN1k_flat]]
+stats = [array([istats[idx10[i],i] for i in range(101)]) for istats in [psI1k_flat, pdf1dN1k_flat, pdf2dN1k_flat]]
 ######################################
 #stats = [psI_flat, pdf1dN_flat, pdf2dN_flat]
 obss = [psI_flat[1], pdf1dN_flat[1], pdf2dN_flat[1]]
 covIs = [covIpsN, covIpdf1dN, covIpdf2dN]
 
-emulators = [WLanalysis.buildInterpolator(istats[idx_good], params[idx_good]) for istats in stats]
+emulators = [WLanalysis.buildInterpolator(array(istats)[idx_good], params[idx_good]) for istats in stats]
 
 chisq = lambda obs, model, covI: float(mat(obs-model)*covI*mat(obs-model).T)
 
