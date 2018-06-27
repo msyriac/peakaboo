1d44a12caec30dae767535f766023d3b0584efbd
liuxx479 <liuxx479@gmail.com>
2018-06-26 20:41:17 -0400

    MC fix bug

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -6,7 +6,7 @@ import sys, itertools
 import emcee
 
 Nk='10k' # '5ka', '5kb'
-testfn = 'allbins'
+testfn = 'collapse'
 Ngrid = 50
 Nchain = 1000
 try:
@@ -38,20 +38,23 @@ eb1k_dir = stats_dir+'stats_avg_1k/output_eb_5000_s4/'
 
 ###### PS shape:(5, 101, 20)
 psI = array( [load(eb_dir+'ALL_igalXigal_z{0}_z{0}_{1}.npy'.format(iz,Nk)) for iz in z_arr])
-psI1ks = array( [[load(eb1k_dir+'ALL_igalXigal_z{0}_z{0}_1k{1}.npy'.format(iz,ik)) for iz in z_arr] 
-                 for ik in range(10)])
+#psI1ks = array( [[load(eb1k_dir+'ALL_igalXigal_z{0}_z{0}_1k{1}.npy'.format(iz,ik)) for iz in z_arr] 
+                 #for ik in range(10)])
 #psN = array( [load(eb_dir+'ALL_galXgal_z{0}_z{0}_{1}.npy'.format(iz,Nk)) for iz in z_arr])
 
 ##### 1d PDF shape:(5, 101, 27)
 pdf1dN = array( [load(eb_dir+'ALL_gal_pdf_z{0}_sg1.0_{1}.npy'.format(iz,Nk)) for iz in z_arr])
-pdf1dN1ks = array( [[load(eb1k_dir+'ALL_gal_pdf_z{0}_sg1.0_1k{1}.npy'.format(iz,ik)) for iz in z_arr] 
-                 for ik in range(10)])
+#pdf1dN1ks = array( [[load(eb1k_dir+'ALL_gal_pdf_z{0}_sg1.0_1k{1}.npy'.format(iz,ik)) for iz in z_arr] 
+                 #for ik in range(10)])
 
 #### 2d PDF shape:(10, 101, 27, 27)
 pdf2dN = array( [load(eb_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_{2}.npy'.format(z_arr[i],z_arr[j],Nk)) 
                 for i in range(Nz) for j in range(i+1,Nz)])
-pdf2dN1ks = array( [[load(eb1k_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_1k{2}.npy'.format(z_arr[i],z_arr[j], ik)) 
-                for i in range(Nz) for j in range(i+1,Nz)] for ik in range(10)])
+#pdf2dN1ks = array( [[load(eb1k_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_1k{2}.npy'.format(z_arr[i],z_arr[j], ik)) 
+                #for i in range(Nz) for j in range(i+1,Nz)] for ik in range(10)])
+
+########## test collapsed 1d PDF from 2d shape:(5, 101, 27)
+pdf1dN = array([sum(pdf2dN[i],axis=-1) for i in [0,4,7,9] ] + [sum(pdf2dN[-1],axis=-2)])
 
 #####################################
 ###### covariances stats ############
@@ -59,7 +62,7 @@ pdf2dN1ks = array( [[load(eb1k_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_1k{2}.npy'
 
 ##### PS shape:(101,100)
 psI_flat = swapaxes(psI,0,1).reshape(101,-1) 
-psI1k_flat = array([swapaxes(ips,0,1).reshape(101,-1) for ips in psI1ks])
+#psI1k_flat = array([swapaxes(ips,0,1).reshape(101,-1) for ips in psI1ks])
 
 psN_cov = swapaxes(array( [load(ebcov_dir+'ALL_galXgal_z{0}_z{0}.npy'.format(iz)) for iz in z_arr]),0,1).reshape(10000,-1)
 covpsN = cov(psN_cov,rowvar=0)*12.25/2e4
@@ -69,7 +72,7 @@ covIpsN = mat(covpsN).I
 idxt=where(pdf1dN[:,5]>0)#range(10, 20)#
 
 pdf1dN_flat= swapaxes(pdf1dN[idxt[0],:,idxt[1]],0,1).reshape(101,-1) 
-pdf1dN1k_flat = array([swapaxes(ips[idxt[0],:,idxt[1]],0,1).reshape(101,-1) for ips in pdf1dN1ks])
+#pdf1dN1k_flat = array([swapaxes(ips[idxt[0],:,idxt[1]],0,1).reshape(101,-1) for ips in pdf1dN1ks])
 
 pdf1dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_gal_pdf_z{0}_sg1.0.npy'.format(iz)) for iz in z_arr])[idxt[0],:,idxt[1]],0,1).reshape(10000,-1)
 covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
@@ -82,7 +85,7 @@ covIpdf1dN = mat(covpdf1dN).I
 idxt2=where(pdf2dN[:,5]>0)
 
 pdf2dN_flat= swapaxes(pdf2dN,0,1)[:,idxt2[0],idxt2[1],idxt2[2]]
-pdf2dN1k_flat= array([swapaxes(ips,0,1)[:,idxt2[0],idxt2[1],idxt2[2]] for ips in pdf2dN1ks])
+#pdf2dN1k_flat= array([swapaxes(ips,0,1)[:,idxt2[0],idxt2[1],idxt2[2]] for ips in pdf2dN1ks])
 
 pdf2dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.npy'.format(z_arr[i],z_arr[j]))
                              for i in range(Nz) for j in range(i+1,Nz)]),0,1)[:,idxt2[0],idxt2[1],idxt2[2]].reshape(10000,-1)
@@ -93,6 +96,9 @@ covpdf2dN = cov(pdf2dN_cov,rowvar=0)*12.25/2e4
 ###########################################
 covIpdf2dN = mat(covpdf2dN).I
 
+############## test collapsed 1d PDF from 2d
+pdf1dN_covb = array([sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.npy'.format(z_arr[i],z_arr[i+1])),axis=-1) for i in range(4) ] + [sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z2.0_z2.5_sg1.0.npy'),axis=-2)])[idxt[0],:,idxt[1]].T
+covIpdf1dN = mat(covpdf1dN).I
 
 #####################################
 ###### build emulator ###############
@@ -166,13 +172,13 @@ sampler.reset()
 sampler.run_mcmc(pos, Nchain)
 save(stats_dir+'likelihood/MC_pdf1d_%s%s.npy'%(Nk,testfn), sampler.flatchain)
 
-print 'PDF 2D'
-#p0 = (array([ (rand(nwalkers, ndim) -0.5) * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[2,], pool=pool)
-pos, prob, state = sampler.run_mcmc(p0, 100)
-sampler.reset()
-sampler.run_mcmc(pos, Nchain*5)
-save(stats_dir+'likelihood/MC_pdf2d_%s%s.npy'%(Nk,testfn), sampler.flatchain)
+#print 'PDF 2D'
+##p0 = (array([ (rand(nwalkers, ndim) -0.5) * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
+#sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[2,], pool=pool)
+#pos, prob, state = sampler.run_mcmc(p0, 100)
+#sampler.reset()
+#sampler.run_mcmc(pos, Nchain*5)
+#save(stats_dir+'likelihood/MC_pdf2d_%s%s.npy'%(Nk,testfn), sampler.flatchain)
 
 #print 'PS'
 #sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[0,], pool=pool)
