587992ecd2cae6cb7f4fbb9649b339af3fc2ea2b
liuxx479 <liuxx479@gmail.com>
2018-07-19 18:32:51 -0400

    test collapsed 1d PDF from 2d, cov only

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -7,7 +7,7 @@ import emcee
 
 Nk='10k' # '5ka', '5kb'
 Nmin=1e-3
-testfn = 'collapsed_mean_only'
+testfn = 'collapsed_cov_only'
 Ngrid = 50
 Nchain = 1000
 try:
@@ -57,7 +57,7 @@ pdf2dN = array( [load(eb_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0_{2}.npy'.format(
                 #for i in range(Nz) for j in range(i+1,Nz)] for ik in range(10)])
 
 ########## test collapsed 1d PDF from 2d shape:(5, 101, 27), mean
-pdf1dN = array([sum(pdf2dN[i],axis=-1) for i in [0,4,7,9] ] + [sum(pdf2dN[-1],axis=-2)])
+#pdf1dN = array([sum(pdf2dN[i],axis=-1) for i in [0,4,7,9] ] + [sum(pdf2dN[-1],axis=-2)])
 
 #####################################
 ###### covariances stats ############
@@ -78,9 +78,9 @@ idxt=where(pdf1dN[:,5]>Nmin)#range(10, 20)#
 pdf1dN_flat= swapaxes(pdf1dN[idxt[0],:,idxt[1]],0,1).reshape(101,-1) 
 ##pdf1dN1k_flat = array([swapaxes(ips[idxt[0],:,idxt[1]],0,1).reshape(101,-1) for ips in pdf1dN1ks])
 
-pdf1dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_gal_pdf_z{0}_sg1.0.npy'.format(iz)) for iz in z_arr])[idxt[0],:,idxt[1]],0,1).reshape(10000,-1)
-covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
-covIpdf1dN = mat(covpdf1dN).I
+#pdf1dN_cov = swapaxes(array( [load(ebcov_dir+'ALL_gal_pdf_z{0}_sg1.0.npy'.format(iz)) for iz in z_arr])[idxt[0],:,idxt[1]],0,1).reshape(10000,-1)
+#covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
+#covIpdf1dN = mat(covpdf1dN).I
 
 ###### PDF 2D
 idxt2=where(pdf2dN[:,5]>Nmin)
@@ -96,9 +96,9 @@ covIpdf2dN = mat(covpdf2dN).I
 
 ############### test collapsed 1d PDF from 2d, covariance
 
-#pdf1dN_cov = array([sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.npy'.format(z_arr[i],z_arr[i+1])),axis=-1) for i in range(4) ] + [sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z2.0_z2.5_sg1.0.npy'),axis=-2)])[idxt[0],:,idxt[1]].T
-#covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
-#covIpdf1dN = mat(covpdf1dN).I
+pdf1dN_cov = array([sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z{0}_z{1}_sg1.0.npy'.format(z_arr[i],z_arr[i+1])),axis=-1) for i in range(4) ] + [sum(load(ebcov_dir+'ALL_galXgal_2dpdf_z2.0_z2.5_sg1.0.npy'),axis=-2)])[idxt[0],:,idxt[1]].T
+covpdf1dN = cov(pdf1dN_cov,rowvar=0)*12.25/2e4
+covIpdf1dN = mat(covpdf1dN).I
 
 #####################################
 ###### build emulator ###############
