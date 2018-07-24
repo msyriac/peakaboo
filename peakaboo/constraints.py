4eebe395c28872c9ded30551bae628d97d68e2e3
liuxx479 <liuxx479@gmail.com>
2018-07-24 19:33:36 -0400

    clean up constraints.py

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -6,7 +6,7 @@ import sys, itertools
 import emcee
 
 Nk='10k' # '5ka', '5kb'
-Nmin=1e-3 ###### minimum counts in that bin to get included in PDF calculation
+Nmin=0.1 ###### minimum counts in that bin to get included in PDF calculation
 testfn = ''#'collapsed'
 Nchain = 1000
 try:
@@ -145,12 +145,12 @@ ndim=3
 np.random.seed(10025)
 p0 = (array([ (rand(nwalkers, ndim) -0.5) * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
 
-#print 'PS'
-#sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[0,], pool=pool)
-#pos, prob, state = sampler.run_mcmc(p0, 100)
-#sampler.reset()
-#sampler.run_mcmc(pos, Nchain)
-#save(stats_dir+'likelihood/MC_ps_%s%s.npy'%(Nk,testfn), sampler.flatchain)
+print 'PS'
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[0,], pool=pool)
+pos, prob, state = sampler.run_mcmc(p0, 100)
+sampler.reset()
+sampler.run_mcmc(pos, Nchain)
+save(stats_dir+'likelihood/MC_ps_%s%s.npy'%(Nk,testfn), sampler.flatchain)
 
 print 'PDF 1D'
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[1,], pool=pool)
