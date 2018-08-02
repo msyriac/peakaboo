aff29ec3183f69c6db38ffb75d4491e079e22ff2
liuxx479 <liuxx479@gmail.com>
2018-08-02 14:25:36 -0400

    poisson lik

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -17,7 +17,7 @@ Nchain = 1000
 np.random.seed(10026)#
 iscale = 1.0#1e-12 ## rescale the PDF so it has similar magnitude as the power spectrum
 Nmin*=iscale
-testfn = collapse+'Possloglik2_R_Nmin%s_Nchain%i_%s'%(Nmin,Nchain,Nk)#''#
+testfn = collapse+'Possloglik2_tightball_R_Nmin%s_Nchain%i_%s'%(Nmin,Nchain,Nk)#''#
 
 z_arr = arange(0.5,3,0.5)
 Nz = len(z_arr)
@@ -217,10 +217,10 @@ if not plot_only:
     nwalkers=544
     ndim=3
     #p0 = (array([ (rand(nwalkers, ndim) -0.5) * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
-    #p0 = (array([ (rand(nwalkers, ndim) -0.5) * 1e-2 * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
+    p0 = (array([ (rand(nwalkers, ndim) -0.5) * 1e-2 * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
     #p0_ranges=array([[0,0.6],[0.25,0.35],[1.6,2.6]])
-    p0_ranges=array([[-0.2,0.8],[0.2,0.4],[1.3,3.0]])
-    p0=rand(nwalkers,ndim)*(p0_ranges[:,1]-p0_ranges[:,0]).reshape(1,3)+p0_ranges[:,0].reshape(1,3)
+    #p0_ranges=array([[-0.2,0.8],[0.2,0.4],[1.3,3.0]])
+    #p0=rand(nwalkers,ndim)*(p0_ranges[:,1]-p0_ranges[:,0]).reshape(1,3)+p0_ranges[:,0].reshape(1,3)
 
     #print 'rDH',rDH
     #for i in range(len(covIs)):
