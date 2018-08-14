ca0de627a7433254b1302ae6095a7e8d2a5908a6
liuxx479 <liuxx479@gmail.com>
2018-08-14 17:07:50 -0400

    test comb contour bug

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -10,7 +10,7 @@ Nmin=5 ###### minimum counts in that bin to get included in PDF calculation
 Nchain = 500
 iscale = 1e-12 ## rescale the PDF so it has similar magnitude as the power spectrum
 
-Nmin_scale_arr = [[iNmin, iscale] for iNmin in (1,5,10,100,500) for iscale in (1e-10, 1e-11, 1e-12, 1e-13, 1)]
+Nmin_scale_arr = [[iNmin, iscale] for iNmin in (5,10,100,500) for iscale in (1e-10,  1e-12, 1e-13, 1)]
 try:
     Nk = str(sys.argv[1])
     Nmin,iscale=Nmin_scale_arr [int(sys.argv[2])]
