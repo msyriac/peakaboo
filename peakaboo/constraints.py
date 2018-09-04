dd477e94cb92b22b656df99b24cf9321a50f6e04
liuxx479 <liuxx479@gmail.com>
2018-09-04 17:30:52 -0400

    test crosses

--- peakaboo/constraints.py
+++ peakaboo/constraints.py
@@ -8,7 +8,7 @@ import os
 
 tightball = 0
 add_2dpdf = 0
-plot_only = 1
+plot_only = 0
 single_z = 0
 test_cross = 1
 very_wide = 0
@@ -266,7 +266,7 @@ if not plot_only:
     #p0 = (array([ (rand(nwalkers, ndim) -0.5) * array([1, 0.3, 0.3]) + 1]) * fidu_params).reshape(-1,3)
     
     ########## wide
-    p0_ranges=array([[0,0.35],[0.25,0.35],[1.6,2.6]])
+    p0_ranges=array([[0,0.4],[0.25,0.35],[1.6,2.6]])
     ########### very wide
     if very_wide:
         p0_ranges=array([[0.0,1.0],[0.1,0.6],[1.2,3.0]])
